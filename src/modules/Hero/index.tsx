dc5c58d60effbc5ab8a467917a86a5fb9c970843
RenatoLagos <renato.lagos.a@gmail.com>
2025-12-02 19:06:05 +0100

    feat: add Hero section component to display personal information and link to CV.

--- src/modules/Hero/index.tsx
+++ src/modules/Hero/index.tsx
@@ -5,7 +5,7 @@ import { Container } from "@components/Container";
 import { Socials } from "@components/Socials";
 import { PaineTowers } from "@static/images";
 
-import Resume from "@static/Renato Agustin Lagos Albornoz - Resume.pdf";
+import Resume from "@static/CV_Renato_Lagos_Professional_Design.pdf";
 import { FadeIn } from "@utils/animations/FadeIn";
 
 export const Hero: FC = () => {
